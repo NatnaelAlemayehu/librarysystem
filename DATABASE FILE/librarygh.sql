f1bb71cc4f6632a540310022704be48fcc04756e
NatnaelAlemayehu <natnaelzewdu0@gmail.com>
2021-12-04 00:24:01 +0300

    new changes

--- DATABASE FILE/librarygh.sql
+++ DATABASE FILE/librarygh.sql
@@ -1,20 +1,21 @@
 -- phpMyAdmin SQL Dump
--- version 4.2.11
--- http://www.phpmyadmin.net
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 21, 2021 at 02:36 PM
--- Server version: 5.6.21
--- PHP Version: 5.6.3
+-- Generation Time: Dec 03, 2021 at 10:23 PM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 8.0.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `librarygh`
@@ -24,8 +25,7 @@ DELIMITER $$
 --
 -- Procedures
 --
-CREATE DEFINER=`root`@`localhost` PROCEDURE `generate_due_list`()
-    NO SQL
+CREATE DEFINER=`root`@`localhost` PROCEDURE `generate_due_list` ()  NO SQL
 SELECT I.issue_id, M.email, B.isbn, B.title
 FROM book_issue_log I INNER JOIN member M on I.member = M.username INNER JOIN book B ON I.book_isbn = B.isbn
 WHERE DATEDIFF(CURRENT_DATE, I.due_date) >= 0 AND DATEDIFF(CURRENT_DATE, I.due_date) % 5 = 0 AND (I.last_reminded IS NULL OR DATEDIFF(I.last_reminded, CURRENT_DATE) <> 0)$$
@@ -35,28 +35,46 @@ DELIMITER ;
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `adminuser`
+--
+
+CREATE TABLE `adminuser` (
+  `id` int(100) NOT NULL,
+  `username` varchar(300) NOT NULL,
+  `password` varchar(200) NOT NULL,
+  `name` varchar(150) NOT NULL,
+  `email` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `adminuser`
+--
+
+INSERT INTO `adminuser` (`id`, `username`, `password`, `name`, `email`) VALUES
+(1, 'admin', 'd033e22ae348aeb5660fc2140aec35850c4da997', 'admin', 'admin@gmail.com');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `book`
 --
 
-CREATE TABLE IF NOT EXISTS `book` (
+CREATE TABLE `book` (
   `isbn` char(13) NOT NULL,
   `title` varchar(80) NOT NULL,
   `author` varchar(80) NOT NULL,
   `category` varchar(80) NOT NULL,
-  `price` int(4) unsigned NOT NULL,
-  `copies` int(10) unsigned NOT NULL
+  `copies` int(10) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `book`
 --
 
-INSERT INTO `book` (`isbn`, `title`, `author`, `category`, `price`, `copies`) VALUES
-('6900152484440', 'V for Vendetta', 'Alan Moore', 'Comics', 299, 13),
-('9782616052277', 'X-Men: God Loves, Man Kills', 'Chris', 'Comics', 399, 33),
-('9783161484100', 'Mike Tyson : Undisputed Truth', 'Larry Sloman, Mike Tyson', 'Sports', 299, 19),
-('9789996245442', 'When Breath Becomes Air', 'Paul Kalanithi', 'Medical', 515, 9),
-('9885691200700', 'The Great Gatsby', 'F. Scott Fitzgerald', 'Fiction', 420, 20);
+INSERT INTO `book` (`isbn`, `title`, `author`, `category`, `copies`) VALUES
+('6900152484440', 'V for Vendetta', 'Alan Moore', 'Comics', 13),
+('9782616052277', 'X-Men: God Loves, Man Kills', 'Chris', 'Comics', 32),
+('9789996245442', 'When Breath Becomes Air', 'Paul Kalanithi', 'Medical', 8);
 
 -- --------------------------------------------------------
 
@@ -64,35 +82,73 @@ INSERT INTO `book` (`isbn`, `title`, `author`, `category`, `price`, `copies`) VA
 -- Table structure for table `book_issue_log`
 --
 
-CREATE TABLE IF NOT EXISTS `book_issue_log` (
-`issue_id` int(11) NOT NULL,
+CREATE TABLE `book_issue_log` (
+  `issue_id` int(11) NOT NULL,
   `member` varchar(20) NOT NULL,
   `book_isbn` varchar(13) NOT NULL,
   `due_date` date NOT NULL,
   `last_reminded` date DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `book_issue_log`
+--
+
+INSERT INTO `book_issue_log` (`issue_id`, `member`, `book_isbn`, `due_date`, `last_reminded`) VALUES
+(3, 'Neba', '9789996245442', '0000-00-00', NULL),
+(4, 'Neba', '9789996245442', '0000-00-00', NULL),
+(5, 'Neba', '9789996245442', '0000-00-00', NULL),
+(6, 'Neba', '9789996245442', '0000-00-00', NULL),
+(7, 'Neba', '9789996245442', '0000-00-00', NULL),
+(10, 'Neba', '9789996245442', '0000-00-00', NULL),
+(12, 'Neba', '9789996245442', '0000-00-00', NULL),
+(14, 'Neba', '9789996245442', '0000-00-00', NULL),
+(16, 'zed', '9782616052277', '2021-12-08', NULL),
+(17, 'zed', '9789996245442', '2021-12-09', NULL);
 
 --
 -- Triggers `book_issue_log`
 --
-DELIMITER //
-CREATE TRIGGER `issue_book` BEFORE INSERT ON `book_issue_log`
- FOR EACH ROW BEGIN
-	SET NEW.due_date = DATE_ADD(CURRENT_DATE, INTERVAL 7 DAY);
-    UPDATE member SET balance = balance - (SELECT price FROM book WHERE isbn = NEW.book_isbn) WHERE username = NEW.member;
+DELIMITER $$
+CREATE TRIGGER `issue_book` BEFORE INSERT ON `book_issue_log` FOR EACH ROW BEGIN
+	SET NEW.due_date = DATE_ADD(CURRENT_DATE, INTERVAL 5 DAY);   
     UPDATE book SET copies = copies - 1 WHERE isbn = NEW.book_isbn;
     DELETE FROM pending_book_requests WHERE member = NEW.member AND book_isbn = NEW.book_isbn;
 END
-//
+$$
 DELIMITER ;
-DELIMITER //
-CREATE TRIGGER `return_book` BEFORE DELETE ON `book_issue_log`
- FOR EACH ROW BEGIN
-    UPDATE member SET balance = balance + (SELECT price FROM book WHERE isbn = OLD.book_isbn) WHERE username = OLD.member;
+DELIMITER $$
+CREATE TRIGGER `return_book` BEFORE DELETE ON `book_issue_log` FOR EACH ROW BEGIN    
     UPDATE book SET copies = copies + 1 WHERE isbn = OLD.book_isbn;
 END
-//
+$$
 DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `faculty`
+--
+
+CREATE TABLE `faculty` (
+  `id` int(11) NOT NULL,
+  `username` varchar(20) NOT NULL,
+  `password` char(40) NOT NULL,
+  `name` varchar(80) NOT NULL,
+  `email` varchar(80) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `faculty`
+--
+
+INSERT INTO `faculty` (`id`, `username`, `password`, `name`, `email`) VALUES
+(7, 'Neba', '76dfc85b8201ec01ac539b49ea6e636eea7f5c3a', 'Neba Roland Ngwa', 'nebaroland9@gmail.com'),
+(12, 'GGG', '60ea5b5295b3c2d4b93fedde853f4dc502d67883', 'Godlove', 'nebagodloveshu@gmail.com'),
+(14, 'Shu', '60ea5b5295b3c2d4b93fedde853f4dc502d67883', 'Neba Godlove Shu', 'nebagodloveshubuna@gmail.com'),
+(15, 'lans', 'ec0224fca34f2df61f615f7ab649460924b66f17', 'lans', 'lans@gmail.com'),
+(16, 'sandra', 'f36a98d9ad435a69048f4c4357d2e4f179cdfdde', 'sandra', 'sandra@gmail.com'),
+(18, 'sirri', 'c3018dc646dfd24a57f6edf7e3a02ae7ad242762', 'sirri', 'sirri@gmail.com');
 
 -- --------------------------------------------------------
 
@@ -100,11 +156,11 @@ DELIMITER ;
 -- Table structure for table `librarian`
 --
 
-CREATE TABLE IF NOT EXISTS `librarian` (
-`id` int(11) NOT NULL,
+CREATE TABLE `librarian` (
+  `id` int(11) NOT NULL,
   `username` varchar(20) NOT NULL,
   `password` char(40) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `librarian`
@@ -119,27 +175,36 @@ INSERT INTO `librarian` (`id`, `username`, `password`) VALUES
 -- Table structure for table `member`
 --
 
-CREATE TABLE IF NOT EXISTS `member` (
-`id` int(11) NOT NULL,
+CREATE TABLE `member` (
+  `id` int(11) NOT NULL,
   `username` varchar(20) NOT NULL,
   `password` char(40) NOT NULL,
   `name` varchar(80) NOT NULL,
-  `email` varchar(80) NOT NULL,
-  `balance` int(4) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+  `email` varchar(80) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `member`
+--
+
+INSERT INTO `member` (`id`, `username`, `password`, `name`, `email`) VALUES
+(7, 'Neba', '76dfc85b8201ec01ac539b49ea6e636eea7f5c3a', 'Neba Roland Ngwa', 'nebaroland9@gmail.com'),
+(9, 'rolandneba', '60ea5b5295b3c2d4b93fedde853f4dc502d67883', 'Neba Roland Ngwa', 'neba@gmail.com'),
+(17, 'lans', 'e84179ca811823479ec0bfcc0ca362284426de3d', 'lans', 'lans@gmail.com'),
+(18, 'alhaji', 'c3018dc646dfd24a57f6edf7e3a02ae7ad242762', 'alhaji', 'alhaji@gmail.com'),
+(19, 'bitu', '474336ef4f53415f5167a24dd8ef45ba700decd1', 'bitu', 'bitu@gmail.com'),
+(20, 'zed', '132318311cfb706be8612458109b1fbf3b9612ce', 'zed', 'zed@gmail.com');
 
 --
 -- Triggers `member`
 --
-DELIMITER //
-CREATE TRIGGER `add_member` AFTER INSERT ON `member`
- FOR EACH ROW DELETE FROM pending_registrations WHERE username = NEW.username
-//
+DELIMITER $$
+CREATE TRIGGER `add_member` AFTER INSERT ON `member` FOR EACH ROW DELETE FROM pending_registrations WHERE username = NEW.username
+$$
 DELIMITER ;
-DELIMITER //
-CREATE TRIGGER `remove_member` AFTER DELETE ON `member`
- FOR EACH ROW DELETE FROM pending_book_requests WHERE member = OLD.username
-//
+DELIMITER $$
+CREATE TRIGGER `remove_member` AFTER DELETE ON `member` FOR EACH ROW DELETE FROM pending_book_requests WHERE member = OLD.username
+$$
 DELIMITER ;
 
 -- --------------------------------------------------------
@@ -148,12 +213,20 @@ DELIMITER ;
 -- Table structure for table `pending_book_requests`
 --
 
-CREATE TABLE IF NOT EXISTS `pending_book_requests` (
-`request_id` int(11) NOT NULL,
+CREATE TABLE `pending_book_requests` (
+  `request_id` int(11) NOT NULL,
   `member` varchar(20) NOT NULL,
   `book_isbn` varchar(13) NOT NULL,
-  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+  `time` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `pending_book_requests`
+--
+
+INSERT INTO `pending_book_requests` (`request_id`, `member`, `book_isbn`, `time`) VALUES
+(7, 'Neba', '9789996245442', '2021-12-03 12:30:04'),
+(8, 'ngwa', '6900152484440', '2021-12-03 14:38:46');
 
 -- --------------------------------------------------------
 
@@ -161,87 +234,167 @@ CREATE TABLE IF NOT EXISTS `pending_book_requests` (
 -- Table structure for table `pending_registrations`
 --
 
-CREATE TABLE IF NOT EXISTS `pending_registrations` (
+CREATE TABLE `pending_registrations` (
   `username` varchar(20) NOT NULL,
   `password` char(40) NOT NULL,
   `name` varchar(80) NOT NULL,
   `email` varchar(80) NOT NULL,
   `balance` int(4) NOT NULL,
-  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+  `time` timestamp NOT NULL DEFAULT current_timestamp(),
+  `category` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `pending_registrations`
 --
 
-INSERT INTO `pending_registrations` (`username`, `password`, `name`, `email`, `balance`, `time`) VALUES
-('christine', '9d4e1e23bd5b727046a9e3b4b7db57bd8d6ee684', 'Christine', 'christine400eer@gmail.com', 999, '2021-03-21 08:29:00'),
-('steeve', '5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8', 'Steeve Rogers', 'thisissteeve69@gmail.com', 1500, '2021-03-21 12:14:53');
+INSERT INTO `pending_registrations` (`username`, `password`, `name`, `email`, `balance`, `time`, `category`) VALUES
+('christine', '9d4e1e23bd5b727046a9e3b4b7db57bd8d6ee684', 'Christine', 'christine400eer@gmail.com', 999, '2021-03-21 08:29:00', 'Faculty'),
+('gmail', 'f2ba2bf8330c51114cbc1b15dfe07c202e92b82e', 'gmail', 'gmail@gmail.com', 0, '2021-12-03 13:31:33', 'Staff'),
+('lans', 'ec0224fca34f2df61f615f7ab649460924b66f17', 'lans', 'lans@gmail.com', 0, '2021-12-03 13:36:11', 'Faculty'),
+('ngwa', 'c3018dc646dfd24a57f6edf7e3a02ae7ad242762', 'ngwa', 'ngwa@gmail.com', 0, '2021-12-03 14:37:44', 'Staff'),
+('sandra', 'f36a98d9ad435a69048f4c4357d2e4f179cdfdde', 'sandra', 'sandra@gmail.com', 0, '2021-12-03 13:37:54', 'Faculty'),
+('shu', 'b5f6543428a52fcaf446870611b3c8573a0dd088', 'shu', 'shu@gmail.com', 0, '2021-12-03 13:32:16', 'Staff'),
+('sirri', 'c3018dc646dfd24a57f6edf7e3a02ae7ad242762', 'sirri', 'sirri@gmail.com', 0, '2021-12-03 13:41:26', 'Faculty'),
+('steeve', '5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8', 'Steeve Rogers', 'thisissteeve69@gmail.com', 1500, '2021-03-21 12:14:53', 'Student');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `staff`
+--
+
+CREATE TABLE `staff` (
+  `id` int(11) NOT NULL,
+  `username` varchar(20) NOT NULL,
+  `password` char(40) NOT NULL,
+  `name` varchar(80) NOT NULL,
+  `email` varchar(80) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `staff`
+--
+
+INSERT INTO `staff` (`id`, `username`, `password`, `name`, `email`) VALUES
+(7, 'Neba', '76dfc85b8201ec01ac539b49ea6e636eea7f5c3a', 'Neba Roland Ngwa', 'nebaroland9@gmail.com'),
+(8, 'gmail', 'f2ba2bf8330c51114cbc1b15dfe07c202e92b82e', 'gmail', 'gmail@gmail.com'),
+(9, 'shu', 'b5f6543428a52fcaf446870611b3c8573a0dd088', 'shu', 'shu@gmail.com'),
+(11, 'ngwa', 'c3018dc646dfd24a57f6edf7e3a02ae7ad242762', 'ngwa', 'ngwa@gmail.com');
 
 --
 -- Indexes for dumped tables
 --
 
 --
+-- Indexes for table `adminuser`
+--
+ALTER TABLE `adminuser`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `book`
 --
 ALTER TABLE `book`
- ADD PRIMARY KEY (`isbn`);
+  ADD PRIMARY KEY (`isbn`);
 
 --
 -- Indexes for table `book_issue_log`
 --
 ALTER TABLE `book_issue_log`
- ADD PRIMARY KEY (`issue_id`);
+  ADD PRIMARY KEY (`issue_id`);
+
+--
+-- Indexes for table `faculty`
+--
+ALTER TABLE `faculty`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`),
+  ADD UNIQUE KEY `email` (`email`);
 
 --
 -- Indexes for table `librarian`
 --
 ALTER TABLE `librarian`
- ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `username` (`username`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
 
 --
 -- Indexes for table `member`
 --
 ALTER TABLE `member`
- ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `username` (`username`), ADD UNIQUE KEY `email` (`email`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`),
+  ADD UNIQUE KEY `email` (`email`);
 
 --
 -- Indexes for table `pending_book_requests`
 --
 ALTER TABLE `pending_book_requests`
- ADD PRIMARY KEY (`request_id`);
+  ADD PRIMARY KEY (`request_id`);
 
 --
 -- Indexes for table `pending_registrations`
 --
 ALTER TABLE `pending_registrations`
- ADD PRIMARY KEY (`username`), ADD UNIQUE KEY `email` (`email`);
+  ADD PRIMARY KEY (`username`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- Indexes for table `staff`
+--
+ALTER TABLE `staff`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`),
+  ADD UNIQUE KEY `email` (`email`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
+-- AUTO_INCREMENT for table `adminuser`
+--
+ALTER TABLE `adminuser`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT for table `book_issue_log`
 --
 ALTER TABLE `book_issue_log`
-MODIFY `issue_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+  MODIFY `issue_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
+--
+-- AUTO_INCREMENT for table `faculty`
+--
+ALTER TABLE `faculty`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+
 --
 -- AUTO_INCREMENT for table `librarian`
 --
 ALTER TABLE `librarian`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
 --
 -- AUTO_INCREMENT for table `member`
 --
 ALTER TABLE `member`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+
 --
 -- AUTO_INCREMENT for table `pending_book_requests`
 --
 ALTER TABLE `pending_book_requests`
-MODIFY `request_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
+  MODIFY `request_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `staff`
+--
+ALTER TABLE `staff`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
